43cc621a74a5f384a7bdca4fa797a031266b4e9c
tavia929 <86624687+tavia929@users.noreply.github.com>
2022-03-09 13:12:35 -0500

    added signature

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Weather from "./Weather";
-
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello!
-        </h1>
-      <Weather />
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.shecodes.io/students/353-octavia-brown" target="_blank">
+            Octavia Brown
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/tavia929/weather-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://objective-elion-e8b66d.netlify.app"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
