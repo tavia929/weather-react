2010d63c9f168b76c14072577b0a696cb402499a
tavia929 <86624687+tavia929@users.noreply.github.com>
2022-03-16 07:25:16 -0400

    removed extra code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -23,15 +23,13 @@ export default function WeatherInfo(props) {
               <WeatherIcon code={props.data.icon} size={52} />
             </div> 
             </div>
-            </div>
-            </div>
-        <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
+    </div>
 
   );
 }
